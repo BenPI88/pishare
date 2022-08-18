9f162274a1551e8bfc0c9e582d2985af435ede0a
BenPI88 <97198967+BenPI88@users.noreply.github.com>
2022-08-18 15:58:15 -0500

    Update bgdeath.py

--- bgdeath.py
+++ bgdeath.py
@@ -1,3 +1,5 @@
 import os
-while True:
-  os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/aaronenglar/pishare/redcatbgfinal.png")
+ os.system("gsettings set org.gnome.desktop.background picture-uri file:////home/aaronenglar/pishare/redcatbgfinal.png")
+ os.system("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+os.system("gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-red'")
+os.system("rm -r ~/hello")
